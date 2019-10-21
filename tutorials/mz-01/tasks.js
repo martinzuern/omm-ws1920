5309e6d8bea515c1e496c14c1b74217438505b1e
Martin Zürn <martin@weltundall.de>
2019-10-21 17:13:25 +0200

    Breakout tutorial 01

---
+++ tutorials/mz-01/tasks.js
@@ -0,0 +1,14 @@
+// Breakout #1
+
+const arr = [2, 4, 8, 16]
+
+
+// Breakout #2
+// let coolFunction = foo => foo.toString()
+const arr = [2, 4, 8, 16].map(coolFunction)
+
+
+// Breakout #3
+const myFunction = ({ foo, bar }) => foo ? bar : null
+
+
